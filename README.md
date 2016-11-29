ab4cb2c5252597804e90c4cd689e6cfcab8766cf
MengJieLi <279037995@qq.com>
2016-11-29 16:29:33 +0800

    提交第一个版本

---
+++ README.md
@@ -0,0 +1,2 @@
+# GameProjServer
+基于 flower 引擎的游戏项目模板的 Server 端
---
+++ remoteServer/tasks/TaskSample.js
@@ -0,0 +1,15 @@
+class TaskSample extends TaskBase {
+
+    constructor(user, fromClient, cmd, msg) {
+        super(user, fromClient, cmd, msg);
+    }
+
+    /**
+     * 开始执行任务
+     * @param cmd
+     * @param msg
+     */
+    startTask(cmd, msg) {
+
+    }
+}
